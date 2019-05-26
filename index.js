35eea12c07dd25d3ffec7307f5763b14b5519734
maxim tsap <salkcid@gmail.com>
2019-05-26 14:06:50 +0300

    detecting anomaies and time converting added

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const fs = require('fs');
 
   sortByAmount(dataGrouped);
 
+  const anomalies = detectAnomalies(dataGrouped, 0.99);
+
+  const anomaliesGrouped = groupBy(anomalies, e => e.category);
+
+  fs.writeFileSync('./resultsGrouped', JSON.stringify(Array.from(anomaliesGrouped.entries()), null, 2));
+  fs.writeFileSync('./results', JSON.stringify(anomalies, null, 2));
 })();
 
 function parseCsv(results) {
@@ -47,4 +53,58 @@ function sortByAmount(dataGrouped) {
     const arr = dataGrouped.get(key);
     arr.sort((a, b) => a.amount - b.amount);
   }
+}
+function valueBiggerThan(data, currentKey, value) {
+  const res = [];
+
+  for (const key of data.keys()) {
+    if (key === currentKey) continue;
+
+    const arr = data.get(key);
+
+    const i = arr.findIndex(e => e.amount > value);
+
+    if (i === -1) {
+      res.push({
+        category: key,
+        perc: 1,
+      });
+
+      continue;
+    }
+
+    res.push({
+      category: key,
+      perc: i / arr.length,
+    });
+  }
+
+  return res;
+}
+function detectAnomalies(data, threshold) {
+  const anomalies = [];
+
+  for (const [key, value] of data.entries()) {
+    for (const entry of value) {
+      const comparison = valueBiggerThan(data, key, entry.amount);
+
+      if (comparison[0].perc >= threshold && comparison[1].perc >= threshold) {
+        anomalies.push({
+          category: entry.category,
+          date: convertTime(entry.time, entry.date),
+          amount: entry.amount,
+        })
+      }
+    }
+  }
+
+  return anomalies;
+}
+function convertTime(hoursShift, startDate) {
+  const val = startDate.slice(0, 10).split('.')
+  const start = new Date(`${val[2]}-${val[1]}-${val[0]}`);
+
+  start.setHours(start.getHours() + +hoursShift);
+
+  return start.toGMTString();
 }
